034bfc9d84cbc9026910ddd070711615fe2ecf05
Adriel <69807379+Adriel92lopez@users.noreply.github.com>
2026-01-06 15:44:28 -0300

    Instalacion completa
    pude hacer que se instale en el celular, pero tengo problemas a la hora de ejecutar el programa directamente no abre es algo para trabajar

--- app/src/main/java/com/example/comesinlmites/Adapter/ComidaAdapter.java
+++ app/src/main/java/com/example/comesinlmites/Adapter/ComidaAdapter.java
@@ -1,5 +1,17 @@
 package com.example.comesinlmites.Adapter;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.comesinlmites.R;
+
+import java.util.List;
+
 public class ComidaAdapter extends RecyclerView.Adapter<ComidaAdapter.ViewHolder> {
 
     private List<String> comidas;
