2ab17d8e1033a8bc22a23c345ca29bd1493913ce
Adriel <69807379+Adriel92lopez@users.noreply.github.com>
2026-01-18 17:31:06 -0300

    cambiar algunas funciones
    trate de corregir errores pero todavía la app no inicia

--- app/src/main/java/com/example/comesinlmites/Controlador/MainActivity.java
+++ app/src/main/java/com/example/comesinlmites/Controlador/MainActivity.java
@@ -3,22 +3,42 @@ package com.example.comesinlmites.Controlador;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.comesinlmites.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Toolbar
         setSupportActionBar(findViewById(R.id.toolbar));
 
-        getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.fragment_container, new HomeFragment())
-                .commit();
+        // NavController
+        NavController navController =
+                Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+
+        // Configuración del AppBar
+        appBarConfiguration = new AppBarConfiguration
+                .Builder(navController.getGraph())
+                .build();
+
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController =
+                Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
-
